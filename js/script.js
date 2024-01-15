5af43a1d1260540659a588d268cd5e54b031bd0e
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-15 19:44:08 +0100

    add form input elements injected on DOM by JS

--- js/script.js
+++ js/script.js
@@ -5,13 +5,16 @@ const countdownElement = document.getElementById('countdown');
 const numberListElement = document.getElementById('number-list');
 const form = document.getElementById('answers-form');
 const message = document.getElementById('message');
-const inputs = document.querySelectorAll('input');
+const inputGroup = document.getElementById('input-group');
 
 //* Declaring info variables
 const min = 1;
 const max = 100;
 const totalNumbers = 5;
 const time = 5;
+
+const inputs = [];
+
 
 countdownElement.innerText = time;
 
@@ -25,7 +28,11 @@ let items = '';
 
 for (let i = 0; i < numbers.length; i++) {
     const number = numbers[i];
-    items += `<li class="fs-4">${number}</li>`
+    items += `<li class="fs-4">${number}</li>`;
+    inputs.push(`<input type="number" class="form-control border border-success" min="${min}" max="${max}" required>`);
 }
 
 numberListElement.innerHTML = items;
+inputGroup.innerHTML = inputs.join('')
+
+
