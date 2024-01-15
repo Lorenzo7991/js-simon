5e4c561f1cee46980786347efcbfafb058d75340
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-15 19:29:54 +0100

    add getRandomNumbers function, add printing on DOM function

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,14 @@
 console.log('F-JS-OK');
+
+//* Different random numbers functions
+const getDifferentRandomNumbers = (min, max, totalNumbers) => {
+
+    const extractedNumbers = [];
+
+    while (extractedNumbers.length < totalNumbers) {
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (!extractedNumbers.includes(randomNumber)) extractedNumbers.push(randomNumber);
+    }
+
+    return extractedNumbers;
+}
--- js/script.js
+++ js/script.js
@@ -1,2 +1,31 @@
 console.log('S-JS-OK');
 
+//* Recovering DOM elements
+const countdownElement = document.getElementById('countdown');
+const numberListElement = document.getElementById('number-list');
+const form = document.getElementById('answers-form');
+const message = document.getElementById('message');
+const inputs = document.querySelectorAll('input');
+
+//* Declaring info variables
+const min = 1;
+const max = 100;
+const totalNumbers = 5;
+const time = 5;
+
+countdownElement.innerText = time;
+
+//* Generating random numbers
+const numbers = getDifferentRandomNumbers(min, max, totalNumbers);
+// Checking function output
+console.log(numbers);
+
+//* Injecting random numbers on DOM
+let items = '';
+
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    items += `<li class="fs-4">${number}</li>`
+}
+
+numberListElement.innerHTML = items;
